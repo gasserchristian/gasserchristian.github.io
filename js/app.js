748b77e11b701112c532dbfec0f84b67ef527625
Chrsitian <christian.marc.gasser@hotmail.com>
2020-06-26 13:20:11 +0200

    attribution

--- js/app.js
+++ js/app.js
@@ -12,9 +12,7 @@ const icon = L.divIcon({
 var carte = L.map('viewer').setView([46.9484, 7.4437], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   maxZoom: 30,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-  	'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-  	'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  attribution: '',
   id: 'mapbox/streets-v11',
   tileSize: 512,
   zoomOffset: -1
